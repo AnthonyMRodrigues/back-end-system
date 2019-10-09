14d77c1607646bfc3ff9fc97df070d1766c73dc7
Anthony Rodrigues <matheus.tere7@gmail.com>
2019-10-09 14:21:30 -0300

    Update models.py
    Feature: Adding Enum for currency
    
    Deleting multiple price fields and using an enum to show price, currency and total price of an order,

--- back-end-app/models.py
+++ back-end-app/models.py
@@ -4,27 +4,25 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 
-class Categories(models.Model):
+class Category(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=200)
-    description = models.CharField(max_length=200)
+    description = models.CharField(max_length=200, blank=True)
     created_at = models.TimeField(auto_now_add=True)
     updated_at = models.TimeField(auto_now=True)
-    deleted_at = models.TimeField()
+    deleted_at = models.TimeField(null=True, blank=True)
 
 
-class Products(models.Model):
+class Product(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=200)
-    description = models.CharField(max_length=200)
+    description = models.CharField(max_length=200,blank=True)
     created_at = models.TimeField(auto_now_add=True)
     updated_at = models.TimeField(auto_now=True)
-    deleted_at = models.TimeField()
-    category_id = models.ForeignKey(Categories, on_delete=models.DO_NOTHING)
+    deleted_at = models.TimeField(null=True, blank=True)
+    category_id = models.ForeignKey(Category, on_delete=models.DO_NOTHING)
     user_id = models.ForeignKey(User, on_delete=models.DO_NOTHING)
-    usd_price = models.DecimalField(decimal_places=2, max_digits=15)
-    real_price = models.DecimalField(decimal_places=2, max_digits=15)
-    euro_price = models.DecimalField(decimal_places=2, max_digits=15)
+    price = models.DecimalField(decimal_places=2, max_digits=15)
 
 
 class Order(models.Model):
@@ -32,15 +30,27 @@ class Order(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.DO_NOTHING)
     created_at = models.TimeField(auto_now_add=True)
     updated_at = models.TimeField(auto_now=True)
-    deleted_at = models.TimeField()
+    deleted_at = models.TimeField(null=True, blank=True)
+    total_price = models.DecimalField(decimal_places=2, max_digits=15)
 
 
-class OrderItens(models.Model):
+class OrderItem(models.Model):
+    BRAZILIAN_REAL = 1
+    AMERICAN_DOLLAR = 2
+    EURO = 3
+
+    currency_types = [
+        (BRAZILIAN_REAL, 'Brazilian Real'),
+        (AMERICAN_DOLLAR, 'American Dollar'),
+        (EURO, 'Euro'),
+    ]
+
     id = models.AutoField(primary_key=True)
-    product_id = models.ForeignKey(Products, on_delete=models.DO_NOTHING)
+    product_id = models.ForeignKey(Product, on_delete=models.DO_NOTHING)
     created_at = models.TimeField(auto_now_add=True)
     updated_at = models.TimeField(auto_now=True)
-    usd_price = models.DecimalField(decimal_places=2, max_digits=15)
-    real_price = models.DecimalField(decimal_places=2, max_digits=15)
-    euro_price = models.DecimalField(decimal_places=2, max_digits=15)
+    deleted_at = models.TimeField(null=True, blank=True)
+    price = models.DecimalField(decimal_places=2, max_digits=15)
     order_id = models.ForeignKey(Order, on_delete=models.DO_NOTHING)
+    exchange_rate = models.DecimalField(decimal_places=2, max_digits=15, default=0)
+    currency = models.IntegerField(max_length=5, choices=currency_types, default=BRAZILIAN_REAL)
